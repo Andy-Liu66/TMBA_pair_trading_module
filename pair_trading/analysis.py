e7507bc849b4cbc8c311cfe7ed241f8e67d6c1d8
andy566159@gmail.comgit config --global user.email andy566159@gmail.com <andy566159@gmail.com>
2019-02-14 01:51:17 +0800

    consider trade cost

--- pair_trading/analysis.py
+++ pair_trading/analysis.py
@@ -34,10 +34,11 @@ class Analysis:
             exit_date = temp_table.date.iloc[-1]
             holding_date = (exit_date - entry_date).days
 
-            # 記錄進出場價格
+            # 記錄進出場價格及部位大小
             # 第2個column為價格資料，因為可能是用其他價格進行交易例如：open, high等，因此用index的方式呼叫
             entry_price = temp_table.iloc[0, 1]
             exit_price = temp_table.iloc[-1, 1]
+            position_size = temp_table.entry_exit_points.iloc[0]
 
             # 找出MFE, MAE
             # 扣掉累加的報酬才能找出發生在當筆交易的損益狀況
@@ -46,29 +47,47 @@ class Analysis:
             maximum_adverse_excursion = temp_table['potential_profit'].min()
 
             # 紀錄報酬
-            actual_payoff = temp_table['potential_profit'].iloc[-1]
-            actual_return = actual_payoff/abs(temp_table.holdings.iloc[0])
+            gross_profit = temp_table['potential_profit'].iloc[-1]
+            gross_return = gross_profit/abs(temp_table.holdings.iloc[0])
+            trade_cost = (
+                # 手續費(進出場接收)
+                entry_price * abs(position_size) * self.strategy.cost +
+                exit_price * abs(position_size) * self.strategy.cost +
+                # 交易稅(出場收)
+                exit_price * abs(position_size) * self.strategy.tax_rate
+                )
+            net_profit = gross_profit - trade_cost
+            net_return = net_profit/abs(temp_table.holdings.iloc[0])
 
             # 將資料整理成dataframe
             temp_result = pd.DataFrame({
                 'entry_date': entry_date,
                 'exit_date': exit_date,
                 'holding_date': holding_date,
+                'position_size': position_size,
                 'entry_price': entry_price,
                 'exit_price': exit_price,
-                'actual_payoff': actual_payoff,
-                'actual_return': actual_return,
-                'maximum_favorable_excursion': maximum_favorable_excursion,
-                'maximum_adverse_excursion': maximum_adverse_excursion
+                'gross_profit': gross_profit,
+                'gross_return': gross_return,
+                'trade_cost': trade_cost,
+                'net_profit': net_profit,
+                'net_return': net_return,
+                # 考量手續費後的MFE, MAE
+                'maximum_favorable_excursion': maximum_favorable_excursion - trade_cost,
+                'maximum_adverse_excursion': maximum_adverse_excursion - trade_cost
             }, columns=[
                 # 需指定columns否則順序會跑掉
                 'entry_date',
                 'exit_date',
                 'holding_date',
+                'position_size',
                 'entry_price',
                 'exit_price',
-                'actual_payoff',
-                'actual_return',
+                'gross_profit',
+                'gross_return',
+                'trade_cost',
+                'net_profit',
+                'net_return',
                 'maximum_favorable_excursion',
                 'maximum_adverse_excursion'
             ], index=[i])
