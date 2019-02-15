0c9749e99be990d03bb1f3d860bd09c6f129f930
andy566159@gmail.comgit config --global user.email andy566159@gmail.com <andy566159@gmail.com>
2019-02-15 16:18:40 +0800

    add summary function, plotting function, and doc string

--- pair_trading/analysis.py
+++ pair_trading/analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 class Analysis:
     
@@ -97,5 +98,167 @@ class Analysis:
         return trade_result
     
     def run(self):
+        # 做多交易結果
         self.stock_to_buy_trade_result = self.__parse_trade_result(buy_or_short='buy')
+
+        # 做空交易結果
         self.stock_to_sellshort_trade_result = self.__parse_trade_result(buy_or_short='short')
+        
+        # 總結果(做多與做空為一個配對，只儲存以下分析需要用到的變數)
+        total_trade_result = pd.DataFrame([])
+        total_trade_result['net_profit'] = (
+            self.stock_to_buy_trade_result.net_profit +
+            self.stock_to_sellshort_trade_result.net_profit
+        )
+
+        total_trade_result['net_return'] = (
+            # 報酬率用相加的有點奇怪(但目前仍使用相加)
+            self.stock_to_buy_trade_result.net_return +
+            self.stock_to_sellshort_trade_result.net_return
+        )
+
+        total_trade_result['holding_date'] = (
+            self.stock_to_buy_trade_result.holding_date +
+            self.stock_to_sellshort_trade_result.holding_date
+        )/2
+
+        total_trade_result['maximum_favorable_excursion'] = (
+            self.stock_to_buy_trade_result.maximum_favorable_excursion +
+            self.stock_to_sellshort_trade_result.maximum_favorable_excursion
+        )
+
+        total_trade_result['maximum_adverse_excursion'] = (
+            self.stock_to_buy_trade_result.maximum_adverse_excursion +
+            self.stock_to_sellshort_trade_result.maximum_adverse_excursion
+        )
+        self.total_trade_result = total_trade_result
+    
+    def summary(self, select_result='total'):
+        '''
+        total - 顯示總交易結果
+        buy - 顯示做多交易結果
+        sellshort - 顯示做空交易結果
+        '''
+        
+        if select_result == 'buy':
+            trade_result = self.stock_to_buy_trade_result
+        elif select_result == 'sellshort':
+            trade_result = self.stock_to_sellshort_trade_result
+        elif select_result == 'total':
+            trade_result = self.total_trade_result
+        
+        # 總獲利金額
+        total_profit = trade_result.net_profit.sum()
+        
+        # 平均報酬
+        average_return = trade_result.net_return.mean()
+        
+        # 勝率
+        total_trade_num = len(trade_result)
+        win_trade_num = sum(trade_result.net_profit > 0)
+        winning_rate = win_trade_num/total_trade_num
+        
+        # 最大獲利回撤
+        accumulate_profit = trade_result.net_profit.cumsum()
+        max_drowdown = max(np.maximum.accumulate(accumulate_profit) - accumulate_profit)
+        
+        # 平均持有天數
+        average_holding_days = trade_result.holding_date.mean()
+        
+        summary = pd.DataFrame({
+            'total_profit': total_profit,
+            'average_return': average_return,
+            'winning_rate': winning_rate,
+            'max_drowdown': max_drowdown,
+            'average_holding_days': average_holding_days
+        }, columns=[
+            'total_profit',
+            'average_return',
+            'winning_rate',
+            'max_drowdown',
+            'average_holding_days'
+        ], index=[0])
+        
+        summary = summary.apply(lambda x: round(x, 4))
+        return summary
+
+    def plot_equity_curve(self, select_result='total', figsize=(16, 8)):
+        '''
+        total - 顯示總交易結果
+        buy - 顯示做多交易結果
+        sellshort - 顯示做空交易結果
+        '''
+        
+        if select_result == 'buy':
+            trade_result = self.stock_to_buy_trade_result
+        elif select_result == 'sellshort':
+            trade_result = self.stock_to_sellshort_trade_result
+        elif select_result == 'total':
+            trade_result = self.total_trade_result
+        else:
+            raise Exception('Wrong input of select_result!')
+        
+        # 累積獲利
+        accumulate_profit = trade_result.net_profit.cumsum()
+        
+        # 找出創新高的index(要畫綠點)
+        new_highest_index = []
+        for i in range(len(accumulate_profit)):
+            current_accumulate_profit = accumulate_profit.iloc[i]
+            if i == 0:
+                new_highest = accumulate_profit.iloc[i]
+            if (current_accumulate_profit > new_highest) and (current_accumulate_profit > 0):
+                new_highest = current_accumulate_profit
+                new_highest_index.append(i)
+        
+        plt.figure(figsize=figsize)
+        # 權益曲線
+        plt.plot(accumulate_profit, c='black')
+        # 創新高的點
+        plt.scatter(new_highest_index,
+                    accumulate_profit[new_highest_index], c='#02ff0f', s=70)
+        plt.title('Equity Curve - {}'.format(select_result), size=20)
+        plt.xlabel('Trade Number', size=15)
+        plt.ylabel('NTD', size=15)
+        plt.xticks(size=13)
+        plt.yticks(size=13);
+    
+    def plot_profit_and_loss_per_trade(self, select_result='total', figsize=(16, 8)):
+        '''
+        total - 顯示總交易結果
+        buy - 顯示做多交易結果
+        sellshort - 顯示做空交易結果
+        '''
+
+        if select_result == 'buy':
+            trade_result = self.stock_to_buy_trade_result
+        elif select_result == 'sellshort':
+            trade_result = self.stock_to_sellshort_trade_result
+        elif select_result == 'total':
+            trade_result = self.total_trade_result
+        else:
+            raise Exception('Wrong input of select_result!')
+        
+        index = trade_result.index
+        plt.figure(figsize=figsize)
+        # 實際獲利與虧損
+        plt.bar(trade_result.net_profit[trade_result.net_profit > 0].index,
+                trade_result.net_profit[trade_result.net_profit > 0],
+                label='Profit', color='red')
+        plt.bar(trade_result.net_profit[trade_result.net_profit < 0].index,
+                trade_result.net_profit[trade_result.net_profit < 0],
+                label='Loss', color='green')
+        # 潛在獲利與虧損
+        plt.bar(trade_result.index,
+                trade_result.maximum_favorable_excursion,
+                color='red', alpha=0.2, label='Possible Profit')
+        plt.bar(trade_result.index,
+                trade_result.maximum_adverse_excursion,
+                color='green', alpha=0.2, label='Possible Loss')
+        
+        plt.legend(fontsize=13)
+        plt.title('Profit and Loss per trade - {}'.format(select_result), size=20)
+        plt.xlabel('Trade Number', size=15)
+        plt.ylabel('NTD', size=15)
+        plt.xticks(size=13)
+        plt.yticks(size=13);
